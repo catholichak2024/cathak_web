07ef7bcc80757946d6e6fd2725984896a0633c1a
E-hyeon <liojung518@gmail.com>
2024-10-21 23:19:18 +0900

    [feat] 라우팅 설정- 상세페이지, 수업 검색페이지

---
+++ src/pages/DetailClass/Styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+  color: #333;
+
+  h1 {
+    font-size: 2.5rem;
+    margin: 0;
+    padding: 20px;
+    text-align: center;
+  }
+`;
---
+++ src/pages/SearchClass/SearchClass.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './Styles';
+
+const SearchClass: React.FC = () => {
+  return (
+    <S.Layout>
+        <h1>Welcome to the Search Page</h1>
+    </S.Layout>
+  );
+};
+
+export default SearchClass;
