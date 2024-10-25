dc69dbf9c0b159cdec16ab1f05f923b72c4f1b3e
E-hyeon <liojung518@gmail.com>
2024-10-26 03:44:39 +0900

    사용자 상태변경

--- src/recoil/types/userdetail.ts
+++ src/recoil/types/userdetail.ts
@@ -3,6 +3,7 @@ export interface userInfoType {
     userid: number;  //웹에서 부여한 사용자번호
     id: string; //사용자가 입력한 아이디
     studentid: number;
+    major_type: string;
     major: string; // 전공
     doubleMajor?: string; // 복수전공일 경우
     minor?: string; // 부전공일 경우
