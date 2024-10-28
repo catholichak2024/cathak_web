b9dd4a1dfd4218db44353a9380013f7003ededb9
E-hyeon <liojung518@gmail.com>
2024-10-29 01:40:05 +0900

    firebase수정

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,14 @@ import { getAuth } from 'firebase/auth'; // Authentication 서비스 가져오�
 import { getFirestore } from 'firebase/firestore'; // Firestore 서비스 가져오기
 import { getStorage } from 'firebase/storage'; // Cloud Storage 서비스 가져오기
 
+
+console.log("API Key:", process.env.REACT_APP_API_KEY);
+console.log("Auth Domain:", process.env.REACT_APP_AUTH_DOMAIN);
+console.log("Project ID:", process.env.REACT_APP_PROJECT_ID);
+console.log("Storage Bucket:", process.env.REACT_APP_STORAGE_BUCKET);
+console.log("Messaging Sender ID:", process.env.REACT_APP_MESSAGING_SENDER_ID);
+console.log("App ID:", process.env.REACT_APP_APP_ID);
+
 // Firebase 설정 (환경 변수에서 가져오기)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY, // 환경 변수에서 API Key 가져오기
