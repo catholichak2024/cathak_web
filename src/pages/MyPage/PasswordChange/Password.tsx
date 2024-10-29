7a2e71db2145fe07f1bd90ce12954b315fdc7db6
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-10-30 08:31:59 +0900

    마이페이지 비번 변경

--- src/pages/MyPage/PasswordChange/Password.tsx
+++ src/pages/MyPage/PasswordChange/Password.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'; 
 import * as S from './Styles';
 import Header from '../../../components/Header/Header';
 import { useNavigate } from 'react-router-dom';
--- src/recoil/states/PasswordChangestates.ts
+++ src/recoil/states/PasswordChangestates.ts
@@ -10,3 +10,4 @@ export const passwordChangeState = atom<PasswordChangeRequest>({
 
 
 
+
