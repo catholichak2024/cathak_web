25bfd4a4e7eb59f8228f75b5d41e522839bacc59
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-10-30 03:26:05 +0900

    테스트

--- src/pages/MyPage/Styles.ts
+++ src/pages/MyPage/Styles.ts
@@ -38,7 +38,6 @@ export const UserName = styled.div`
   margin-left: 170px;   
 `;
 
-
 export const Middle = styled.div`
   position: absolute;
   top: 70%;              
@@ -48,8 +47,6 @@ export const Middle = styled.div`
   width: 80%;
   align-items: center;
 `;
-
-
 
 export const Bottom = styled.div`
     align-items: center; 
@@ -124,4 +121,98 @@ export const passwordBox = styled.div`
   cursor:pointer;
 `;
 
+export const PopupOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
 
+export const PopupContent = styled.div<{ type: string }>` 
+  background: white;
+  padding: 10px;
+  border-radius: 10px;
+  text-align: center;
+  width: 320px;
+  height: 194px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PopupTitle = styled.h2<{ type: string }>`
+  margin: 0;
+  font-size: 18px;
+  color: ${({ type }) => type === 'delete' ? '#FF3B3B' : 'black'};  
+`;
+
+export const PopupDescription = styled.p`
+  margin: 5px 0 10px; 
+  font-size: 14px;
+  color: #626262
+`;
+
+export const ButtonContainer = styled.div`
+  justify-content: center;
+  width: auto;
+  margin-top: 10px;
+  margin-right: 2px;
+`;
+
+export const CloseButton = styled.button<{ type: string }>`
+  background-color: #F1F1F1;
+  border: none;
+  width: 106px;
+  height: 38px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-right: 8px;
+  right: 10%;
+
+  &:hover {
+    background-color: ${({ type }) => type === 'delete' ? '#FF3B3B' : '#D6E8F1'}; 
+    color: ${({ type }) => type === 'delete' ? 'white' : 'black'};
+  }
+`;
+
+export const ConfirmButton = styled.button<{ type: string }>`
+  background-color: #F1F1F1; 
+  border: none;
+  width: 106px;
+  height: 38px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 8px;
+
+  &:hover {
+    background-color: ${({ type }) => type === 'delete' ? '#FF3B3B' : '#D6E8F1'}; 
+    color: ${({ type }) => type === 'delete' ? 'white' : 'black'};
+  }
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 10px;
+  background-color: white;
+`;
+
+export const LogoutIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  filter: invert(24%) sepia(90%) saturate(3665%) hue-rotate(352deg) brightness(94%) contrast(106%);
+  /* 아이콘을 빨간색으로 보이게 하는 필터 */
+  &:hover {
+    filter: invert(48%) sepia(93%) saturate(4269%) hue-rotate(352deg) brightness(90%) contrast(98%);
+    /* 호버 시 조금 더 진한 빨간색으로 변경 */
+  }
+`;
