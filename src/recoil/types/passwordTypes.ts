de3d3ab4b84571b9b60885f743587e32a2875dbf
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-10-30 13:40:14 +0900

    재 커밋 전공/비번

--- src/recoil/types/passwordTypes.ts
+++ src/recoil/types/passwordTypes.ts
@@ -1,4 +1,9 @@
-export interface PasswordRequest {
-  number: string,
-  id: string
-}
+import { atom } from 'recoil';
+import { PasswordChangeRequest } from '../types/passwordChangeTypes';
+
+export const passwordChangeState = atom<PasswordChangeRequest>({
+    key: "passwordChangeState",
+    default: {
+      pw: ''
+    },
+});
